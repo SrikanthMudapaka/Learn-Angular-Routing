942870ac7371f56ff6fdd52491fbfac0f4b0cade
SrikanthMudapaka <srikanthsr.m9@gmail.com>
2020-09-04 19:01:44 +0530

    routing address

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PayrollComponent } from './payroll/payroll.component';
+import { HrhelpdeskComponent } from './hrhelpdesk/hrhelpdesk.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LearningComponent } from './learning/learning.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PayrollComponent,
+    HrhelpdeskComponent,
+    ProfileComponent,
+    LearningComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
